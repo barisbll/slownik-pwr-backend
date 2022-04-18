b74652a0d776826e53f11ef05f2f930cbee951ac
Baris Balli <barisbll.dev@gmail.com>
2022-04-18 14:14:32 +0200

    wtf

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 module.exports = {
   mongodbSecret:
-    "mongodb+srv://<secret>:<secret>@slownikpwr.pnaxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://<username>:<password>@slownikpwr.pnaxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
